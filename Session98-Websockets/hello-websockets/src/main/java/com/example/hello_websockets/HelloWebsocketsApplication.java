3c9948ac0d9b62c40abdfee30212b67c1e8a9ab8
Le Nguyen Minh Kha <lenguyenminhkha1606@gmail.com>
2025-08-04 00:49:22 +0700

    learn websokect

---
+++ Session98-Websockets/hello-websockets/src/main/java/com/example/hello_websockets/HelloWebsocketsApplication.java
@@ -0,0 +1,13 @@
+package com.example.hello_websockets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWebsocketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWebsocketsApplication.class, args);
+	}
+
+}
---
+++ Session98-Websockets/hello-websockets/src/test/java/com/example/hello_websockets/HelloWebsocketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hello_websockets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWebsocketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
